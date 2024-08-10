688db970772fc63ad50e3c9d8e1b2d6d780a0202
Alex-Apex <agomez@apexsystems.com>
2024-08-09 18:54:54 -0600

    chore: added support to MSSQL server.

---
+++ database/db.js
@@ -0,0 +1,27 @@
+const sql = require('mssql');
+
+// Configure the database connection using environment variables
+const dbConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
+    options: {
+        encrypt: true, // Use encryption (recommended if you use Azure)
+        enableArithAbort: true, // Terminate queries that result in overflow or divide-by-zero errors
+    },
+};
+
+// Create a pool of connections to be reused
+const poolPromise = new sql.ConnectionPool(dbConfig)
+    .connect()
+    .then(pool => {
+        console.log('Connected to SQL Server');
+        return pool;
+    })
+    .catch(err => console.log('Database Connection Failed! Bad Config: ', err));
+
+module.exports = {
+    sql, 
+    poolPromise
+};
