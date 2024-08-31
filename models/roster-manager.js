4939a34b2ff5e26cd4a81c11f940397dc5cf7054
Alex-Apex <agomez@apexsystems.com>
2024-08-30 19:06:30 -0600

    Style: styled the RosterList

--- models/roster-manager.js
+++ models/roster-manager.js
@@ -11,8 +11,44 @@ class RosterManager {
    */
   async getRoster() {
     const query = `
-    SELECT * 
-    FROM Employees
+   WITH EmployeeHierarchy AS
+  (
+      SELECT
+          id,
+          name,
+          username,
+          current_title,
+          supervisor_id,
+          practice_id
+      FROM
+          EMPLOYEES
+      WHERE
+          id = 2  -- the Id of the director you're starting from
+  
+      UNION ALL
+  
+      SELECT
+          E.id,
+          E.name,
+          E.username,
+          E.current_title,
+          E.supervisor_id,
+          E.practice_id
+      FROM
+          EMPLOYEES E
+      INNER JOIN EmployeeHierarchy EH ON E.supervisor_id = EH.id
+  )
+  SELECT  EHE.id,
+          EHE.name,
+          EHE.username,
+          EHE.current_title,
+          EHE.supervisor_id,
+          ESup.name AS supervisor_name,
+          EHE.practice_id, P.name AS practice_name 
+  FROM EmployeeHierarchy EHE INNER JOIN Practices P
+    ON EHE.practice_id = P.id
+    INNER JOIN Employees ESup
+    ON EHE.supervisor_id = ESup.id;
     `;
     //----
     try {
